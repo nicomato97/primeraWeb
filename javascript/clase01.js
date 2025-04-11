103c3909acd60ef3af6ec09fa8694ae896a0d0b8
nicomato97 <nicomato97@gmail.com>
2025-04-11 14:42:10 -0500

    Moví los archivos a la raíz para que funcionen con GitHub Pages

---
+++ javascript/clase01.js
@@ -0,0 +1,36 @@
+/*
+var nombre = window.prompt("Dime tu nombre:", "");
+var edad = window.prompt("Dime tu edad:", "");
+var numero1 = window.prompt("Dime tu numero 1:", "");
+var numero2 = window.prompt("Dime tu numero 2:", "");
+document.write(nombre);
+document.write('<br>');
+document.write(edad);
+document.write('<br>');
+document.write(numero1);
+document.write('<br>');
+document.write(numero2);
+document.write('<br>');
+
+
+if(numero1 > numero2){
+    console.log('El número mayor es: ', numero1)
+} else{
+    console.log('El número mayor es: ', numero2)
+}
+*/
+
+let x=1;
+while (x <= 10){
+    console.log('Soy el número: ', x)
+    x++;
+}
+
+for(y=1; y<=10; y++){
+    console.log('Soy el número: ', y)
+}
+
+
+
+
+
